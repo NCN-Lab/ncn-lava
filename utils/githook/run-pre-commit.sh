57684be78edf8c46de1ba08e258508a4ac871ec8
Monkin <monkinsmurf@gmail.com>
2024-09-09 15:12:28 +0100

    Update: Pyproject.toml project settings to match Mesquita's dissertation config

--- utils/githook/run-pre-commit.sh
+++ utils/githook/run-pre-commit.sh
@@ -3,6 +3,6 @@
 set -e
 echo "Running pre-commit checks ..."
 DIR=`git rev-parse --show-toplevel`
-source $DIR/utils/githook/run-lint-flake.sh
+# source $DIR/utils/githook/run-lint-flake.sh
 source $DIR/utils/githook/run-pytest.sh
 source $DIR/utils/githook/fix-whitespace.sh
